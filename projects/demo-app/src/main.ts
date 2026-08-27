7e024c873348e1d466821785570ab217783c8f76
Sumit Kokal <sumit.kokal@nitorinfotech.com>
2026-08-27 13:11:06 +0530

    changes

--- projects/demo-app/src/main.ts
+++ projects/demo-app/src/main.ts
@@ -9,6 +9,7 @@ import Aura from '@primeuix/themes/aura';
 
 import { AppComponent } from './app/app.component';
 import { routes as showcaseRoutes } from './app/features/showcase/showcase.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   { path: '', redirectTo: 'showcase', pathMatch: 'full' },
@@ -17,7 +18,7 @@ const routes: Routes = [
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZoneChangeDetection(),provideAnimations(),
+    provideZoneChangeDetection(), provideAnimations(),
     provideRouter(routes),
 
     providePrimeNG({
