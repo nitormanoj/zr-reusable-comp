1b70d6e53873879e92d2a86e0961d7d4648d4aa3
nitormanoj <manoj.bobade@nitorinfotech.com>
2026-09-01 12:05:48 +0530

    commit for conflict

--- projects/ui-table/src/lib/buttons/zr-button.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ZrButtonDirective } from './zr-button.directive';
-
-describe('ZrButtonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ZrButtonDirective();
-    expect(directive).toBeTruthy();
-  });
-});
